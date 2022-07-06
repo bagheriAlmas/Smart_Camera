d5693819d4dbc01da8be7d88e2861e68ff5ee40a
hojat.ghasemi <hojat72elect@gmail.com>
2022-07-05 23:12:38 -0400

    Asking for all the mandatory permissions in the app start

--- app/src/main/java/ca/on/sudbury/hojat/smartcamera/MainActivity.kt
+++ app/src/main/java/ca/on/sudbury/hojat/smartcamera/MainActivity.kt
@@ -1,11 +1,56 @@
 package ca.on.sudbury.hojat.smartcamera
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import ca.on.sudbury.hojat.smartcamera.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Asking for permissions
+        if (allPermissionsGranted()) {
+            Toast.makeText(this, "we have permissions", Toast.LENGTH_SHORT).show()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                Constants.REQUIRED_PERMISSIONS,
+                Constants.REQUEST_CODE_PERMISSIONS
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == Constants.REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                // our code
+            } else {
+                Toast.makeText(this, "Permissions not granted by the user", Toast.LENGTH_SHORT)
+                    .show()
+                finish()
+            }
+        }
+
+    }
+
+    private fun allPermissionsGranted() = Constants.REQUIRED_PERMISSIONS.all { permission ->
+        ContextCompat.checkSelfPermission(
+            baseContext, permission
+        ) == PackageManager.PERMISSION_GRANTED
+
     }
 }
